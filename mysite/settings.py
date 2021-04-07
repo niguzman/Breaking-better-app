356f191e8f2e5d8c2517e60ba22e05638492220a
niguzman <niguzman@uc.cl>
2021-04-07 18:27:35 -0400

    Allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://breaking-better-app.herokuapp.com']
+ALLOWED_HOSTS = ['breaking-better-app.herokuapp.com']
 
 
 # Application definition
